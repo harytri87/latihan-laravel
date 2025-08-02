33e2082e52e21b0700866f667417980f6b547fa7
harytri87 <harytri87@gmail.com>
2025-08-02 11:57:08 +0700

    Disable Tailwind dark mode.
    - Disabled dark mode styles in app.css
    - Published and cleaned up error pages to remove dark styling
    - Removed duplicate route names

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(BlogController::class)->group(function () {
 	// Bisa diakses semua orang
-	Route::get('/', 'index')->name('home');
+	Route::get('/', 'index');
 	Route::get('blogs', 'index')->name('home');
 
 	// Biar penamaan lebih simple. Tapi kalo sekiranya terlalu banyak nested, bisa dihapus aja, manual kasih nama yg lengkap
